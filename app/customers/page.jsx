9ff0d71861347389e4cc27bc22655c2e08a393f7
community-vercel <community-vercel>
2025-06-24 12:05:52 -0700

    aa

--- app/customers/page.jsx
+++ app/customers/page.jsx
@@ -189,7 +189,9 @@ export default function Customers() {
                 </div>
                 <div className="flex space-x-2">
           
-        
+    
+
+
                 <Link href={`/payments?customerId=${customer._id}`}>
                     <button className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition">
                       Add Transaction

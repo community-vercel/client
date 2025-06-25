c956c646601ab41470b14fd532d28e093e960aa6
community-vercel <community-vercel>
2025-06-25 10:39:27 -0700

    aa

--- app/payments/page.js
+++ app/payments/page.js
@@ -499,6 +499,7 @@ export default function Transactions() {
                     )
                   );
                 }}
+
                 
                 className="w-full p-3 bg-gray-700 text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400 transition"
                 list="customers"
